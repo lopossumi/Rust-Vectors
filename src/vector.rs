df2938dc0198d35b78a5bd9445ef30051253ff61
Mikko Loponen <mikko.loponen@iki.fi>
2020-07-15 13:04:52 +0300

    add vector operations and tests

--- src/vector.rs
+++ src/vector.rs
@@ -1,5 +1,5 @@
 use core::fmt;
-use std::ops::{Add, Sub, Neg, Mul};
+use std::ops::{Add, Sub, Neg, Mul, Div};
 
 #[derive(Clone, Copy)]
 pub struct Vec3 {
@@ -12,6 +12,36 @@ impl Vec3 {
     pub fn new(x: f64, y: f64, z: f64) -> Vec3 {
         Vec3 { x, y, z }
     }
+
+    pub fn dot(&self, other: Vec3) -> f64 {
+        self.x * other.x 
+        + self.y * other.y 
+        + self.z * other.z
+    }
+
+    pub fn cross(&self, other: Vec3) -> Vec3 {
+        Vec3{
+            x: self.y * other.z - self.z * other.y,
+            y: self.z * other.x - self.x * other.z,
+            z: self.x * other.y - self.y * other.x
+        }
+    }
+
+    pub fn length_squared(&self) -> f64 {
+        self.x * self.x 
+        + self.y * self.y 
+        + self.z * self.z
+    }
+
+    pub fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+
+    pub fn to_rgb(&self) -> (u8, u8, u8) {
+        (self.x.min(255.0).max(0.0) as u8, 
+        self.y.min(255.0).max(0.0) as u8, 
+        self.z.min(255.0).max(0.0) as u8)
+    }    
 }
 
 impl Add<Vec3> for Vec3 {
@@ -49,11 +79,45 @@ impl Neg for Vec3 {
 
 impl Mul<Vec3> for f64 {
     type Output = Vec3;
+    fn mul(self, vector: Vec3) -> Vec3 {
+        Vec3{
+            x: self * vector.x,
+            y: self * vector.y, 
+            z: self * vector.z
+        }
+    }
+}
+
+impl Mul<f64> for Vec3 {
+    type Output = Vec3;
+    fn mul(self, scalar: f64) -> Vec3 {
+        Vec3{
+            x: self.x * scalar,
+            y: self.y * scalar, 
+            z: self.z * scalar
+        }
+    }
+}
+
+// Hadamard product
+impl Mul<Vec3> for Vec3 {
+    type Output = Vec3;
     fn mul(self, other: Vec3) -> Vec3 {
         Vec3{
-            x: self * other.x,
-            y: self * other.y, 
-            z: self * other.z
+            x: self.x * other.x,
+            y: self.y * other.y, 
+            z: self.z * other.z
+        }
+    }
+}
+
+impl Div<f64> for Vec3 {
+    type Output = Vec3;
+    fn div(self, scalar: f64) -> Vec3 {
+        Vec3{
+            x: self.x / scalar,
+            y: self.y / scalar, 
+            z: self.z / scalar
         }
     }
 }
@@ -82,33 +146,95 @@ mod tests {
     fn addition() {
         let vector1 = Vec3::new(1.0, 2.0, 3.0);
         let vector2 = Vec3::new(4.0, 5.0, 6.0);
-        let actual = vector1 + vector2;
+        let result = vector1 + vector2;
         let expected = Vec3::new(5.0, 7.0, 9.0);
-        assert_vec3_equal!(expected, actual);
+        assert_vec3_equal!(expected, result);
     }
 
     #[test]
     fn substraction() {
         let vector1 = Vec3::new(1.0, 2.0, 3.0);
         let vector2 = Vec3::new(4.0, 5.0, 6.0);
-        let actual = vector1 - vector2;
+        let result = vector1 - vector2;
         let expected = Vec3::new(-3.0, -3.0, -3.0);
-        assert_vec3_equal!(expected, actual);
+        assert_vec3_equal!(expected, result);
     }
 
     #[test]
     fn negation() {
         let vector1 = Vec3::new(1.0, 2.0, 3.0);
-        let actual = -vector1; 
+        let result = -vector1; 
         let expected = Vec3::new(-1.0, -2.0, -3.0);
-        assert_vec3_equal!(expected, actual);
+        assert_vec3_equal!(expected, result);
     }
 
     #[test]
     fn multiplication() {
         let vector1 = Vec3::new(1.0, 2.0, 3.0);
-        let actual = 5.0 * vector1;
+        let result1 = 5.0 * vector1;
         let expected = Vec3::new(5.0, 10.0, 15.0);
-        assert_vec3_equal!(expected, actual);
+        assert_vec3_equal!(expected, result1);
+
+        let result2 = vector1 * 5.0;
+        assert_vec3_equal!(expected, result2);
+    }
+
+    #[test]
+    fn hadamard_product() {
+        let vector1 = Vec3::new(1.0, 2.0, 3.0);
+        let vector2 = Vec3::new(4.0, 5.0, 6.0);
+
+        let result = vector1 * vector2;
+        let expected = Vec3::new(4.0, 10.0, 18.0);
+        assert_vec3_equal!(expected, result);
+    }
+
+    #[test]
+    fn division() {
+        let vector = Vec3::new(12.0, 13.5, 15.0);
+        let divisor = 3.0;
+        let result = vector / divisor;
+        let expected = Vec3::new(4.0, 4.5, 5.0);
+        assert_vec3_equal!(expected, result);
+    }
+
+    #[test]
+    fn length() {
+        let vector = Vec3::new(3.0, 4.0, 5.0);
+        let result = vector.length();
+        assert_approx_eq!(7.07168, result, 0.001);
+    }
+
+    #[test]
+    fn length_squared() {
+        let vector = Vec3::new(3.0, 4.0, 5.0);
+        let result = vector.length_squared();
+        assert_approx_eq!(50.0, result, 0.001);
+    }
+
+    #[test]
+    fn dot(){
+        let vector1 = Vec3::new(1.0, 2.0, 3.0);
+        let vector2 = Vec3::new(1.0, 5.0, 7.0);
+        let result = vector1.dot(vector2);
+        assert_approx_eq!(32.0, result, 0.001);
+    }
+
+    #[test]
+    fn cross(){
+        let vector1 = Vec3::new(1.0, 2.0, 3.0);
+        let vector2 = Vec3::new(1.0, 5.0, 7.0);
+        let result = vector1.cross(vector2);
+        let expected = Vec3::new(-1.0, -4.0, 3.0);
+        assert_vec3_equal!(expected, result);
+
+    }
+
+    #[test]
+    fn to_rgb(){
+        let vector = Vec3::new(-1.0, 2.0, 300.0);
+        let result = vector.to_rgb();
+        let expected = (0u8, 2u8, 255u8);
+        assert_eq!(expected, result);
     }
 }
